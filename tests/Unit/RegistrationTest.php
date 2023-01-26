69e252a124fece833ed2ac912edfb2e84ca95a36
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-01-26 16:11:09 -0600

    more assert test

--- tests/Unit/RegistrationTest.php
+++ tests/Unit/RegistrationTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Livewire;
 use Tests\TestCase;
 
@@ -38,9 +39,78 @@ class RegistrationTest extends TestCase
             ->set('password', 'secret')
             ->set('passwordConfirmation', 'secret')
             ->call('register')
-            ->assertHasErrors('email')
+            ->assertHasErrors(['email' => 'required'])
         ;
+    }
 
-        // 
+    /** @test */
+    public function email_is_valid()
+    {
+        Livewire::test('auth.register')
+            ->set('name', 'name')
+            ->set('email', 'name')
+            ->set('password', 'secret')
+            ->set('passwordConfirmation', 'secret')
+            ->call('register')
+            ->assertHasErrors(['email' => 'email'])
+        ;
+    }
+
+    /** @test */
+    public function all_emails_are_uniqe()
+    {
+        User::create([
+            'name' => 'name',
+            'email' => 'first@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        Livewire::test('auth.register')
+            ->set('name', 'name')
+            ->set('email', 'first@gmail.com')
+            ->set('password', 'secret')
+            ->set('passwordConfirmation', 'secret')
+            ->call('register')
+            ->assertHasErrors(['email' => 'unique'])
+        ;
+    }
+
+    /** @test */
+    public function password_is_required()
+    {
+        Livewire::test('auth.register')
+            ->set('name', 'name')
+            ->set('email', 'first@gmail.com')
+            ->set('password', '')
+            ->set('passwordConfirmation', 'secret')
+            ->call('register')
+            ->assertHasErrors(['password' => 'required'])
+        ;
+    }
+
+    /** @test */
+    public function password_is_long_enough()
+    {
+        Livewire::test('auth.register')
+            ->set('name', 'name')
+            ->set('email', 'first@gmail.com')
+            ->set('password', 'sec')
+            ->set('passwordConfirmation', 'secret')
+            ->call('register')
+            ->assertHasErrors(['password' => 'min'])
+        ;
+    }
+
+    /** @test */
+    public function password_matches_password_confirmation()
+    {
+        Livewire::test('auth.register')
+            ->set('name', 'name')
+            ->set('email', 'first@gmail.com')
+            ->set('password', 'sec')
+            ->set('passwordConfirmation', 'secret')
+            ->call('register')
+            ->assertHasErrors(['password' => 'same'])
+        ;
     }
 }
